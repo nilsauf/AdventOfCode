631d7a1eaad7e6db8ebc4ac6f1ef0bead4ea3eb7
Nils Aufschläger <mail@nilsauf.de>
2022-07-20 22:50:15 +0200

    finish solution for #1, but too low..

--- 2021/05/solution1.py
+++ 2021/05/solution1.py
@@ -1,20 +1,62 @@
 from collections import namedtuple
+from functools import reduce
+from typing import Iterable
 
 line = namedtuple("line", ["start", "end"])
 point = namedtuple("point", ["X", "Y"])
 
 
 def readLinesFromFile(path):
-    with open("2021/05/input.txt") as reader:
+    with open(path) as reader:
         return tuple(map(lambda x: line(next(x), next(x)), map(lambda x: map(lambda y: point(int(y[0]), int(y[1])), map(lambda y: y.split(','), x)), map(lambda x: filter(lambda y: "," in y, x.split()), reader))))
 
 
-def getVerticalAndHorizontalLines(lines):
-    return tuple(filter(lambda x: x.start.X == x.end.X or x.start.Y == x.end.Y, lines))
+def getIfLineIsHorizontal(line: line):
+    return line.start.Y == line.end.Y
 
 
-lines = readLinesFromFile("")
-vhlines = getVerticalAndHorizontalLines(lines)
+def getIfLineIsVertical(line: line):
+    return line.start.X == line.end.X
 
-print(vhlines[0])
-print(vhlines[1])
+
+def getHorizontalLines(lines):
+    return tuple(filter(getIfLineIsHorizontal, lines))
+
+
+def getVerticalLines(lines):
+    return tuple(filter(getIfLineIsVertical, lines))
+
+
+def getIfLinesCross(first: line, second: line):
+    return getIfLineIsHorizontal(first) != getIfLineIsHorizontal(second) and (first.start.X <= second.start.X <= first.end.X or first.start.X >= second.start.X >= first.end.X) and (second.start.Y <= first.start.Y <= second.end.Y or second.start.Y >= first.start.Y >= second.end.Y)
+
+
+def getPointWhereLinesCross(first: line, second: line):
+    horizontalLine = first if getIfLineIsHorizontal(first) else second
+    verticalLine = first if horizontalLine is second else first
+    return point(horizontalLine.start.Y, verticalLine.start.X)
+
+
+def getCrossingPoints(hLines, vLines):
+    return reduce(lambda x, y: x+y, map(lambda hLine: tuple(map(lambda vLine: getPointWhereLinesCross(hLine, vLine), filter(lambda vLine: getIfLinesCross(hLine, vLine), vLines))), hLines))
+
+
+def distinct(itr: Iterable):
+    alreadyYielded = []
+    for thing in itr:
+        if alreadyYielded.__contains__(thing) == False:
+            alreadyYielded.append(thing)
+            yield thing
+
+
+def count(itr: Iterable):
+    return reduce(lambda x, y: x+1, itr, 0)
+
+
+lines = readLinesFromFile("2021/05/input.txt")
+
+hLines = getHorizontalLines(lines)
+vLines = getVerticalLines(lines)
+countCrossingPoints = count(distinct(getCrossingPoints(hLines, vLines)))
+
+print(countCrossingPoints)
