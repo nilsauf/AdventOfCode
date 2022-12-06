7a345dc72d8bb38d99e8e3a240419bdd2a1c1d6b
Nils Aufschläger <mail@nilsauf.de>
2022-12-06 21:54:48 +0100

    adding input and solution for 02/2022

---
+++ 2022/02/solution.js
@@ -0,0 +1,48 @@
+const fs = require("fs");
+
+const data = fs.readFileSync("input.txt", {
+  encoding: "utf8",
+  flag: "r",
+});
+
+const gameData = data
+  .trim("\r\n")
+  .split("\r\n")
+  .map((line) => line.trim("\r\n").split(" "));
+
+const gameResultPoints1 = {
+  A: { X: 3, Y: 6, Z: 0 },
+  B: { X: 0, Y: 3, Z: 6 },
+  C: { X: 6, Y: 0, Z: 3 },
+};
+
+const sumPoints1 = gameData
+  .map(
+    (game) => gameResultPoints1[game[0]][game[1]] + game[1].charCodeAt(0) - 87
+  )
+  .reduce((acc, val) => acc + val, 0);
+
+console.log("Solution Part 1: ");
+console.log(sumPoints1);
+
+const gameResultPoints2 = {
+  X: 0,
+  Y: 3,
+  Z: 6,
+};
+
+const neededToChooseShapePoints = {
+  A: { X: 3, Y: 1, Z: 2 },
+  B: { X: 1, Y: 2, Z: 3 },
+  C: { X: 2, Y: 3, Z: 1 },
+};
+
+const sumPoints2 = gameData
+  .map(
+    (game) =>
+      gameResultPoints2[game[1]] + neededToChooseShapePoints[game[0]][game[1]]
+  )
+  .reduce((acc, val) => acc + val, 0);
+
+console.log("Solution Part 2: ");
+console.log(sumPoints2);
