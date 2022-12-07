451dc2770ad677951f87eac26481809301bdbe70
Nils Aufschläger <mail@nilsauf.de>
2022-12-07 20:27:13 +0100

    add solution for 03/2022 Part 2

--- 2022/03/solution.js
+++ 2022/03/solution.js
@@ -1,16 +1,9 @@
-import {
-  readInput,
-  sum,
-  isLowerCase,
-  printSolution,
-} from "../../utils/utils.js";
+import * as utils from "../../utils/utils.js";
 
-const data = readInput("2022/03/input.txt");
+const data = utils.readInput().trim("\r\n").split("\r\n");
 
-const sumOfPriorities1 = sum(
+const sumOfPriorities1 = utils.sum(
   data
-    .trim("\r\n")
-    .split("\r\n")
     .map((sack) => [
       sack.slice(0, sack.length / 2).split(""),
       sack.slice(sack.length / 2).split(""),
@@ -18,7 +11,28 @@ const sumOfPriorities1 = sum(
     .map((sackCompartments) =>
       sackCompartments[0].find((item) => sackCompartments[1].includes(item))
     )
-    .map((item) => item.charCodeAt(0) - (isLowerCase(item) ? 96 : 38))
+    .map((item) => item.charCodeAt(0) - (utils.isLowerCase(item) ? 96 : 38))
 );
 
-printSolution(1, sumOfPriorities1);
+utils.printSolution(1, sumOfPriorities1);
+
+const sumOfPriorities2 = utils.sum(
+  data
+    .reduce((array, sack) => {
+      let value = sack.split("");
+      if (array.length == 0 || array[array.length - 1].length == 3) {
+        array.push([value]);
+      } else {
+        array[array.length - 1].push(value);
+      }
+      return array;
+    }, [])
+    .map((sackgroups) =>
+      sackgroups[0].find(
+        (item) => sackgroups[1].includes(item) && sackgroups[2].includes(item)
+      )
+    )
+    .map((item) => item.charCodeAt(0) - (utils.isLowerCase(item) ? 96 : 38))
+);
+
+utils.printSolution(2, sumOfPriorities2);
