d8dd7cb72382f99f076715ac1ecb2c6db04768d2
Nils Aufschläger <mail@nilsauf.de>
2023-12-03 12:16:32 +0100

    adding 2023 Day 2 Part 2

--- 2023/AdventOfCode_2023.Day_2/Program.cs
+++ 2023/AdventOfCode_2023.Day_2/Program.cs
@@ -40,5 +40,43 @@ Console.WriteLine($"Result #1: {result1}");
 
 /// ---------------------------------------------------------------------- ///
 
+int result2 = Data.Input.Split('\n')
+	.Select(GetPowerOfMinimumSet)
+	.Sum();
 
-// Console.WriteLine($"Result #2: {result2}");
+int GetPowerOfMinimumSet(string line)
+{
+	return line.Split(':')
+		.Last()
+		.Split(';')
+		.Select(dataPart => dataPart
+			.Split(',')
+			.Select(draw => Data.GameDataRegex.Match(draw))
+			.Select(drawMatch => (
+				count: int.Parse(drawMatch.Groups[1].Value),
+				color: drawMatch.Groups[2].Value.Trim(' ', '\r', '\n')
+			))
+			.Aggregate(
+				new Dictionary<string, int>() { { "red", 0 }, { "blue", 0 }, { "green", 0 } },
+				(aggregate, newData) =>
+				{
+					aggregate[newData.color] += newData.count;
+					return aggregate;
+				}))
+		.Aggregate(
+			new Dictionary<string, int>() { { "red", 0 }, { "blue", 0 }, { "green", 0 } },
+			(aggregate, newData) =>
+			{
+				if (aggregate["red"] < newData["red"])
+					aggregate["red"] = newData["red"];
+				if (aggregate["blue"] < newData["blue"])
+					aggregate["blue"] = newData["blue"];
+				if (aggregate["green"] < newData["green"])
+					aggregate["green"] = newData["green"];
+				return aggregate;
+			})
+		.Values
+		.Aggregate(1, (aggregate, nextValue) => aggregate * nextValue);
+}
+
+Console.WriteLine($"Result #2: {result2}");
